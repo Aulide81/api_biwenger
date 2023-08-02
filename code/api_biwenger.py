f6741ac5e419e32ddbcb1458af046590385992b6
Aulide81 <emiliojat@gmail.com>
2023-08-02 12:50:16 +0200

    Update api_biwenger.py

--- code/api_biwenger.py
+++ code/api_biwenger.py
@@ -1,4 +1,3 @@
-
 import requests
 import pickle
 import json
@@ -8,12 +7,16 @@ from pprint import pprint
 
 class biwenger:
   
-  def __init__(self, mail, password):
+  def __init__(self, mail, password, id_league=1648876, id_user=9997844):
     self.mail=mail
     self.password=password
+    self.id_league=str(id_league)
+    self.id_user=str(id_user)    
     self.token=self.__get_token__(mail,password)
+    self.users=self.__get_users__()
     self.balance=None
-    
+
+
   def __get_token__(self, mail, password):
     url = "https://biwenger.as.com/api/v2/auth/login"
     login_data = dict(email = mail, password = password)
@@ -26,15 +29,20 @@ class biwenger:
       return("Sesion iniciada correctamente con {mail}".format(mail=self.mail))
     else:
       return("No se ha iniciado sesion")
-
-  def create_balance(self, league=1648876, usuario=9997844):
     
+    
+  def __get_users__(self):
+    url="https://biwenger.as.com/api/v2/league?include=all,-lastAccess&fields=*,standings,tournaments,group,settings(description)"
+    liga = requests.get(url, headers={'authorization':'Bearer '+ self.token, 'x-league': self.id_league, 'x-user': self.id_user})
+    diccionario=liga.json()
+    participantes = diccionario['data']['standings']
+    participantes={j['name']:j['id'] for j in participantes}
+    return(participantes)
+
+  def create_balance(self):
     if self.balance is None:
-      url="https://biwenger.as.com/api/v2/league?include=all,-lastAccess&fields=*,standings,tournaments,group,settings(description)"
-      liga = requests.get(url, headers={'authorization':'Bearer '+ self.token, 'x-league': str(league), 'x-user': str(usuario)})
-      diccionario=liga.json()
-      balance = diccionario['data']['standings']
-      balance={j['name']:{0:50000000} for j in balance}
+      participantes=self.get_users()
+      balance={j:{0:50000000} for j in participantes.keys()}
       balance['__ult.act']=0
       self.balance=balance
       
@@ -47,7 +55,7 @@ class biwenger:
     with open(path, "wb") as file:
       pickle.dump(self.balance,file)
       
-  def update_balance(self,league=1648876, usuario=9997844, limit=500):
+  def update_balance(self, limit=500):
     
     if self.balance is None:
       
@@ -57,7 +65,7 @@ class biwenger:
     else:
       
       url="https://biwenger.as.com/api/v2/league/1648876/board?offset=0&limit={limit}".format(limit=limit)
-      home = requests.get(url, headers={'authorization':'Bearer '+ self.token, 'x-league': str(league), 'x-user': str(usuario)})
+      home = requests.get(url, headers={'authorization':'Bearer '+ self.token, 'x-league': self.id_league, 'x-user': self.id_user})
       diccionario=home.json()
       muro=diccionario['data']
       ult_act=self.balance['__ult.act']
@@ -95,6 +103,7 @@ class biwenger:
 
               
               # Hay "vendedor"?
+              
               if 'from' in mov.keys():
                 vendedor=mov['from']['name']
                 
@@ -118,10 +127,31 @@ class biwenger:
         pass
   
   def get_players(self):
-    url="https://cf.biwenger.com/api/v2/competitions/la-liga/data?lang=es&score=3&callback=jsonp_1465365484"
-    lista_jugadores = requests.get(url)
-    lista_jugadores=json.loads(lista_jugadores.text[17:-1])['data']['players']
     
-    df=pd.DataFrame(lista_jugadores)
-    df=df.transpose()
-    return(df)
+    url="https://cf.biwenger.com/api/v2/competitions/la-liga/data?lang=es&score=3&callback=jsonp_1465365484"
+    players = requests.get(url)
+    players = json.loads(players.text[17:-1])['data']['players']
+    df_players = pd.DataFrame(players)
+    df_players = df_players.transpose()
+    df_players.index = df_players.index.astype(int)
+    return(df_players)
+  
+  def team(self, user = None):
+    if user is None:
+      user=self.id_user
+    url="https://biwenger.as.com/api/v2/user/{user}?fields=*,account(id),players(id,owner),lineups(round,points,count,position),league(id,name,competition,type,mode,marketMode,scoreID),market,seasons,offers,lastPositions".format(user=user)
+    team = requests.get(url, headers={'authorization':'Bearer '+ self.token, 'x-league': self.id_league, 'x-user': self.id_user})
+    team = team.json()['data']['players']
+    team = pd.Series({player['id']:player['owner']['price'] for player in team})
+    team = pd.DataFrame(team,columns=["Pvp"])
+    players = self.get_players()
+    team = team.join(players)
+    
+    team['Beneficio'] = team.price - team.Pvp
+    
+    columnas=['id', 'name','price','Pvp','Beneficio','priceIncrement',
+    'points','position', 'status', 'playedHome', 'playedAway',
+    'fitness', 'pointsHome', 'pointsAway', 'pointsLastSeason',
+    'statusInfo','slug','teamID','fantasyPrice']
+    
+    return(team[columnas])
